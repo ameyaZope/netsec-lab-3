2121c247629d7507a5cee318fecd9860fae0fd9c
ameyaZope <zopeameya@gmail.com>
2024-04-08 20:25:55 -0400

    Enabling Encryption between client and server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ameyaZope/netsec-lab-3
 
 go 1.21.6
+
+require golang.org/x/crypto v0.22.0 // indirect
--- jumproxy.go
+++ jumproxy.go
@@ -1,6 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/binary"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"io"
@@ -9,7 +16,148 @@ import (
 	"os"
 	"strconv"
 	"sync"
+
+	"golang.org/x/crypto/pbkdf2"
 )
+
+func generateKey(secret []byte, saltInput []byte) ([]byte, []byte) {
+	var salt []byte
+	if saltInput == nil {
+		// salt = make([]byte, 16)
+		salt = []byte("abcdef0123456789")
+		if _, err := rand.Read(salt); err != nil {
+			log.Fatalf("Error During Salt Generation for Key Generation %v", err)
+		}
+	} else {
+		salt = saltInput
+	}
+
+	key := pbkdf2.Key(secret, salt, 4096, 32, sha256.New) // 256-bit key
+	return key, salt
+}
+
+func encrypt(plaintext []byte, key []byte) ([]byte, []byte, error) {
+	aesCipher, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, nil, err
+	}
+	gcm, err := cipher.NewGCM(aesCipher)
+	if err != nil {
+		return nil, nil, err
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	_, errRandNonce := io.ReadFull(rand.Reader, nonce)
+	if errRandNonce != nil {
+		return nil, nil, errRandNonce
+	}
+	ciphertext := make([]byte, 0, len(plaintext)+gcm.Overhead())
+	ciphertext = gcm.Seal(ciphertext, nonce, plaintext, nil)
+
+	return ciphertext, nonce, nil
+}
+
+func decrypt(ciphertext []byte, key []byte, nonce []byte) ([]byte, error) {
+	aesCipher, errCipher := aes.NewCipher(key)
+	if errCipher != nil {
+		log.Printf("[decrypt] Calculating Cipher %v\n", errCipher)
+		return nil, errCipher
+	}
+	gcm, errGcm := cipher.NewGCM(aesCipher)
+	if errGcm != nil {
+		log.Printf("[decrypt] Calculating GCM %v\n", errGcm)
+		return nil, errGcm
+	}
+
+	plaintext := make([]byte, len(ciphertext))
+	plaintext, errOpen := gcm.Open(plaintext, nonce, ciphertext, nil)
+	if errOpen != nil {
+		log.Printf("[decrypt] gcm.Open failed %v\n", errOpen)
+		log.Fatalf("[decrypt] ciphertext=%x key=%x nonce=%x\n", ciphertext, key, nonce)
+		return nil, errOpen
+	}
+	return plaintext, nil
+}
+
+// Encrypts the data and sends it, including the nonce with the ciphertext.
+func sendEncrypted(conn net.Conn, data []byte, key []byte) error {
+	ciphertext, nonce, errEncrypt := encrypt(data, key)
+	if errEncrypt != nil {
+		return errEncrypt
+	}
+	// Assuming a simple protocol: send the nonce size, nonce, and then ciphertext.
+	nonceSizeBuf := make([]byte, 8)
+	binary.BigEndian.PutUint64(nonceSizeBuf, uint64(len(nonce)))
+	_, errWriteNonceSize := conn.Write(nonceSizeBuf)
+	if errWriteNonceSize != nil {
+		log.Printf("Client [ERROR] Writing NonceSize: %v\n", errWriteNonceSize)
+		return errWriteNonceSize
+	}
+	_, errWriteNonce := conn.Write(nonce)
+	if errWriteNonce != nil {
+		log.Printf("Client [ERROR] Writing Nonce: %v\n", errWriteNonce)
+		return errWriteNonce
+	}
+
+	cipherTextSize := len(ciphertext)
+	sizeBuf := make([]byte, 8)
+	binary.BigEndian.PutUint64(sizeBuf, uint64(cipherTextSize))
+	_, errWriteCipherTextSize := conn.Write(sizeBuf)
+	if errWriteCipherTextSize != nil {
+		log.Printf("Client [ERROR] Writing CipherTextSize: %v\n", errWriteCipherTextSize)
+	}
+
+	_, errWriteCipherText := conn.Write(ciphertext)
+	if errWriteCipherText != nil {
+		log.Printf("Client [ERROR] Writing CipherText: %v\n", errWriteCipherText)
+		return errWriteCipherText
+	}
+	return nil
+}
+
+// Reads encrypted data, extracts the nonce, and decrypts the data.
+func receiveDecrypted(conn net.Conn, key []byte) ([]byte, error) {
+	nonceSizeBuf := make([]byte, 8)
+	_, errReadNonceSize := conn.Read(nonceSizeBuf)
+	nonceSize := int64(binary.BigEndian.Uint64(nonceSizeBuf))
+	log.Printf("[reciveDecrypted] Recieved NonceSize %d\n", nonceSize)
+	if errReadNonceSize != nil {
+		log.Printf("[recieveDecrypt] Error Reading NonceSize : %v\n", errReadNonceSize)
+		return nil, errReadNonceSize
+	}
+
+	nonce := make([]byte, nonceSize)
+	_, errReadNonce := conn.Read(nonce)
+	log.Printf("[reciveDecrypted] Recieved NonceSize %x\n", nonce)
+	if errReadNonce != nil {
+		log.Printf("[recieveDecrypt] Error Reading Nonce : %v\n", errReadNonce)
+		return nil, errReadNonce
+	}
+
+	cipherTextSizeBuf := make([]byte, 8)
+	_, errReadCipherTextSize := conn.Read(cipherTextSizeBuf)
+	cipherTextSize := int64(binary.BigEndian.Uint64(cipherTextSizeBuf))
+	log.Printf("[reciveDecrypted] Recieved CipherTextSize %d\n", cipherTextSize)
+	if errReadCipherTextSize != nil {
+		log.Printf("[recieveDecrypt] Error Reading CipherTextSize : %v\n", errReadCipherTextSize)
+		return nil, errReadCipherTextSize
+	}
+
+	// Assuming the remaining data is the ciphertext. In practice, you might want to prefix the data with its size.
+	ciphertext := make([]byte, cipherTextSize)
+	n, errReadCipherText := conn.Read(ciphertext)
+	log.Printf("[reciveDecrypted] Recieved CipherText %x\n", ciphertext)
+	if errReadCipherText != nil {
+		log.Printf("[recieveDecrypt] Error Reading CipherText : %v\n", errReadCipherText)
+		return nil, errReadCipherText
+	}
+
+	plaintext, errDecrpyt := decrypt(ciphertext[:n], key, nonce)
+	if errDecrpyt != nil {
+		log.Printf("[recieveDecrypt] Error Decrypt : %v\n", errDecrpyt)
+		return nil, errDecrpyt
+	}
+	return plaintext, nil
+}
 
 func main() {
 	var keyFileName = flag.String("k", "mykey", "Use the ASCII text passphrase contained in <pwdfile>")
@@ -24,14 +172,25 @@ func main() {
 		destHost = flag.Args()[0]
 		destPort, err = strconv.ParseInt(flag.Args()[1], 10, 64)
 		if err != nil {
-			fmt.Printf("Input Port is not a valid 64 bit number\n")
+			log.Printf("Input Port is not a valid 64 bit number\n")
 		}
-		fmt.Printf("Destination Host : %s\nDestination Port : %d\n", destHost, destPort)
+		log.Printf("Destination Host : %s\nDestination Port : %d\n", destHost, destPort)
 	}
 
-	fmt.Printf("KeyFileName : %s\n", *keyFileName)
+	log.Printf("KeyFileName : %s\n", *keyFileName)
+	passphrase, err := os.ReadFile(*keyFileName)
+
+	/*
+		I am assuming that client is the one who always initates the connection
+		and since this is a jump proxy this assumption is valid. Because of this
+		assumption I need to generate the secure key inside the client first
+		and then send the salt as plain text to the server as the first 16 bytes.
+		The server can then generate the secure key on its side and then start
+		decrypting as nonce, ciphertext
+	*/
+
 	if *listenPort == -1 {
-		fmt.Printf("Client Mode\n")
+		log.Printf("Client Mode\n")
 
 		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", destHost, destPort))
 		if err != nil {
@@ -39,25 +198,44 @@ func main() {
 		}
 		defer conn.Close()
 
+		key, keyError := hex.DecodeString("186c82651c0d565e6d56541fc614036d33e857a143d44f915cca44e3687694ee")
+		if keyError != nil {
+			log.Printf("Proxy: Error Generating Key %v", keyError)
+		}
+
+		// write the salt as the first few bytes
+		// conn.Write(salt)
+
 		// Using WaitGroup to manage goroutines completion
 		var wg sync.WaitGroup
-		wg.Add(2)
+		wg.Add(1)
 
 		// Goroutine for copying server responses to stdout
 		go func() {
 			defer wg.Done()
-			_, err := io.Copy(os.Stdout, conn)
-			if err != nil {
-				log.Printf("Error relaying data from server to stdout: %v", err)
+			for {
+				log.Printf("Client: key = %x\n", key)
+				plaintext, errRecieveDecrypt := receiveDecrypted(conn, key)
+				if errRecieveDecrypt != nil || errRecieveDecrypt == io.EOF {
+					break
+				}
+				os.Stdout.Write(plaintext)
 			}
 		}()
 
 		// Goroutine for reading from stdin and sending to the server
 		go func() {
 			defer wg.Done()
-			_, err := io.Copy(conn, os.Stdin)
-			if err != nil {
-				log.Printf("Error reading from stdin and writing to server: %v", err)
+			for {
+				log.Printf("Client : Awaiting Input From Stdin\n")
+				reader := bufio.NewReader(os.Stdin)
+				plaintext, errReadBytes := reader.ReadBytes('\n')
+				if errReadBytes != nil {
+					log.Printf("Client [ERROR] [Reading Input From Stdin] : %v", errReadBytes)
+					break
+				}
+				sendEncrypted(conn, plaintext, key)
+				log.Printf("Client : Sent Encrpyted Input to Server %s\n", plaintext)
 			}
 		}()
 
@@ -66,56 +244,79 @@ func main() {
 	} else {
 		listener, err := net.Listen("tcp", ":"+fmt.Sprintf("%d", *listenPort))
 		if err != nil {
-			fmt.Println("Error:", err)
+			log.Println("Error:", err)
 			return
 		}
 		defer listener.Close()
 
-		fmt.Printf("Server Mode: listening on port %d\n", *listenPort)
+		log.Printf("Server Mode: listening on port %d\n", *listenPort)
 
 		for {
 			// Accept incoming connections
 			conn, err := listener.Accept()
 			if err != nil {
-				fmt.Println("Error:", err)
+				log.Println("Error:", err)
 				continue
 			}
 
 			// Handle client connection in a goroutine
-			go handleClient(conn, destHost, destPort)
+			go handleClient(conn, destHost, destPort, passphrase)
 		}
 	}
 }
 
-func handleClient(clientConn net.Conn, destHost string, destPort int64) {
+func handleClient(clientConn net.Conn, destHost string, destPort int64, passphrase []byte) {
 	serviceConn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", destHost, destPort))
 	if err != nil {
 		log.Fatalf("Failed to connect to service: %v", err)
 	}
-	defer serviceConn.Close()
-	// Forward data from client to service
+	key, keyError := hex.DecodeString("186c82651c0d565e6d56541fc614036d33e857a143d44f915cca44e3687694ee")
+	if keyError != nil {
+		log.Fatalf("Proxy: Error Generating Key %v", keyError)
+	}
+
 	var wg sync.WaitGroup
 	wg.Add(2)
-	// Forward data from client to service
+	// Accept data from client to service
 	go func() {
 		defer wg.Done()
-		_, err := io.Copy(serviceConn, clientConn)
-		if err != nil {
-			log.Printf("Error relaying data from client to service: %v", err)
+		for {
+			log.Printf("Proxy : Awaiting Data From Client\n")
+			plaintext, errRecieveDecrypt := receiveDecrypted(clientConn, key)
+			if errRecieveDecrypt != nil || errRecieveDecrypt == io.EOF {
+				log.Fatal("Proxy [ERROR] [Recieve Data from Client]: %v", errRecieveDecrypt)
+				break
+			}
+			log.Printf("Proxy : Recieved %s from service\n", plaintext)
+			n, err := serviceConn.Write(plaintext)
+			if err != nil {
+				log.Fatal("[ERROR] Proxy Client to Proxy: %v\n", err)
+			} else {
+				log.Printf("Wrote %d bytes", n)
+			}
+			log.Printf("Proxy : Sent %s to service\n", plaintext)
 		}
+
 	}()
 
-	// Forward data from service to client
+	// Send data from service to client
 	go func() {
 		defer wg.Done()
-		_, err := io.Copy(clientConn, serviceConn)
-		if err != nil {
-			log.Printf("Error relaying data from service to client: %v", err)
+		for {
+			log.Printf("Proxy : Awaiting Data From Service\n")
+			reader := bufio.NewReader(serviceConn)
+			plaintext, err := reader.ReadBytes('\n')
+			if err != nil {
+				log.Printf("[ERROR] Proxy Error Reading Data From Service : %v\n", err)
+			}
+			log.Printf("Proxy : Recieved %s from Service\n", plaintext)
+			sendEncrypted(clientConn, plaintext, key)
+			log.Printf("Proxy : Sent %s to client in encrypted form\n", plaintext)
 		}
-
 	}()
 
 	wg.Wait()
-
+	log.Printf("Proxy : Closing Service and Client Connections\n")
+	serviceConn.Close()
 	clientConn.Close()
 }
